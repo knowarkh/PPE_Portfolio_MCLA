cd40bb1f36ebf13ec33ccff4bea89830ed6bdf95
admin <admin@172.16.120.44>
2018-01-23 16:14:47 +0100

    avec les fichiers de Melody

--- db/Connexion.php
+++ db/Connexion.php
@@ -21,6 +21,70 @@ namespace DB\Connexion {
             }
             return $dbh;
         }
+        
+        static function getStagiaire()
+        {
+            $sql = "SELECT * FROM STAGIAIRE;";
+            $rep = "<table class=\"table table-striped\">";
+            foreach (Connexion::getInstance()->query($sql) as $row) {
+                $rep .= "<tr><td>" . $row["idS"];
+                $rep .= "</td><td>" . $row["nom"];
+                $rep .= "</td><td>" . $row["prenom"];
+                $rep .= "</td><td>" . $row["mail"] . "</td></tr>";
+            }
+            return $rep . "</table>";
+        }
+        
+        //Affiche le tableau des activités validées
+        static function getTableauCompetences($idStagiaire){
+            //requête mySQL permettant de récupérer les données nécéssaires au tableau
+            $sql = "SELECT DISTINCT activite.processus, activite.domaineActivite, activite.denomination, competence.description, validation.contexte, ressource.chemin
+FROM validation, competence, preuve, ressource,activite
+WHERE validation.idS=$idStagiaire
+AND validation.idC = competence.idC
+AND validation.idS = preuve.idS
+AND validation.idC= preuve.idC
+AND ressource.idR= preuve.idR
+AND activite.idA = competence.idA;
+";
+            
+            $rep = "<table class=\"tableauGeneral\"><tr><th>Activite</th><th>Competence</th></tr>";
+            $oldActivite="";
+            $oldCompetence="";
+            
+            
+            foreach (Connexion::getInstance()->query($sql) as $row) {
+                //Création du tableau activité dans un ligne du ableau général si nouvelle activité, sinon ne rien faire
+                if ($oldActivite != $row["denomination"]){
+                    if ($oldActivite != ""){
+                        $rep.="</td></tr></table></td></tr>";
+                    }
+                    $rep.="<tr><td class=\"domaineActivite\">".$row["processus"]."<br/>".$row["domaineActivite"]."</td></tr>";
+                    $rep.="<tr><td>".$row['denomination']."</td>";
+                    $rep.="<td>";
+                    $rep.="<table class=\"tableauCompetence\">";
+                    $rep.="<tr><td>".$row["description"]."</td>";
+                    $rep.="<td>".$row["contexte"]."<br/><a href=\"".$row["chemin"]."\">lien</a>";
+                    
+                    $oldActivite= $row["denomination"];
+                    $oldCompetence=$row["description"];
+                }
+                else{
+                    if ($oldCompetence != $row["description"]){
+                        $rep.="</td></tr><tr><td>".$row["description"]."</td>";
+                        $rep.="<td>".$row["contexte"]."<br/><a href=\"".$row["chemin"]."\">lien</a>";
+                        
+                        $oldCompetence=["description"];
+                    }
+                    else{
+                        $rep.="<br/><a href=\"".$row["chemin"]."\">lien</a>";
+                    }
+                    
+                }
+                
+            }
+            return $rep . "</td></tr></table></td></tr></table>";
+        }
     }
 }
 ?>
