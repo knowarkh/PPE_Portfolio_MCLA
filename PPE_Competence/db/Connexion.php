cb43ba6f87ef7ac81f8bb29df8161f8dca644446
admin <admin@172.16.120.59>
2018-01-23 16:00:11 +0100

    fonction getTableau

--- PPE_Competence/db/Connexion.php
+++ PPE_Competence/db/Connexion.php
@@ -1,27 +1,96 @@
 <?php
+namespace DB\Connexion
+{
 
-namespace DB\Connexion {
-    
-    class Connexion {
-        static function getInstance() {
+    class Connexion
+    {
+
+        static function getInstance()
+        {
             static $dbh = NULL;
-            if ($dbh==NULL) {
-                $dsn = "mysql:host=localhost:3306;dbname=aero";
+            if ($dbh == NULL) {
+                $dsn = "mysql:host=localhost:3306;dbname=competence";
                 $username = "root";
                 $password = "";
-                //Goto project -> properties -> Project Facets and enable both facets
-                //pour expliciter le namespace, on prefixe la classe avec \
-                $options = array (
+                // Goto project -> properties -> Project Facets and enable both facets
+                // pour expliciter le namespace, on prefixe la classe avec \
+                $options = array(
                     \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
                 );
                 try {
-                    $dbh = new \PDO ( $dsn, $username, $password, $options );
-                } catch ( \PDOException $e ) {
+                    $dbh = new \PDO($dsn, $username, $password, $options);
+                } catch (\PDOException $e) {
                     echo "Probleme de connexion", $e;
                 }
             }
             return $dbh;
         }
+
+        static function getStagiaire()
+        {
+            $sql = "SELECT * FROM STAGIAIRE;";
+            $rep = "<table class=\"table table-striped\">";
+            foreach (Connexion::getInstance()->query($sql) as $row) {
+                $rep .= "<tr><td>" . $row["idS"];
+                $rep .= "</td><td>" . $row["nom"];
+                $rep .= "</td><td>" . $row["prenom"];
+                $rep .= "</td><td>" . $row["mail"] . "</td></tr>";
+            }
+            return $rep . "</table>";
+        }
+
+        
+        static function getTableauCompetences($idStagiaire){
+            //requête mySQL permettant de récupérer les données nécéssaires au tableau 
+            $sql = "SELECT DISTINCT activite.processus, activite.domaineActivite, activite.denomination, competence.description, validation.contexte, ressource.chemin
+FROM validation, competence, preuve, ressource,activite
+WHERE validation.idS=$idStagiaire
+AND validation.idC = competence.idC
+AND validation.idS = preuve.idS
+AND validation.idC= preuve.idC
+AND ressource.idR= preuve.idR
+AND activite.idA = competence.idA;
+";
+            
+            
+            $rep = "<table class=\"tableauGeneral\"><tr><th>Activite</th><th>Competence</th></tr>";
+            $oldActivite="";
+            $oldCompetence="";
+           
+            
+            foreach (Connexion::getInstance()->query($sql) as $row) {
+                //Création du tableau activité dans un ligne du ableau général si nouvelle activité, sinon ne rien faire
+                if ($oldActivite != $row["denomination"]){
+                    if ($oldActivite != ""){
+                        $rep.="</td></tr></table></td></tr>";
+                    }
+                    $rep.="<tr><td class=\"domaineActivite\">".$row["processus"]."<br/>".$row["domaineActivite"]."</td></tr>";
+                    $rep.="<tr><td>".$row['denomination']."</td>"; 
+                    $rep.="<td>";
+                    $rep.="<table class=\"tableauCompetence\">";
+                    $rep.="<tr><td>".$row["description"]."</td>";   
+                    $rep.="<td>".$row["contexte"]."<br/><a href=\"".$row["chemin"]."\">lien</a>";  
+                    
+                    $oldActivite= $row["denomination"];
+                    $oldCompetence=$row["description"];             
+                }
+                else{
+                    if ($oldCompetence != $row["description"]){
+                        $rep.="</td></tr><tr><td>".$row["description"]."</td>";
+                        $rep.="<td>".$row["contexte"]."<br/><a href=\"".$row["chemin"]."\">lien</a>";
+                        
+                        $oldCompetence=["description"];
+                    }
+                    else{
+                        $rep.="<br/><a href=\"".$row["chemin"]."\">lien</a>";
+                    }
+       
+                }      
+            
+        }
+        return $rep . "</td></tr></table></td></tr></table>";
+        }
     }
 }
+
 ?>
--- PPE_Competence/vue/index.php
+++ PPE_Competence/vue/index.php
@@ -1,5 +1,9 @@
+<?php
+use DB\Connexion\Connexion;
+?>
 <html>
 <head>
+<link href="stylesheet.css" rel="stylesheet">
 <!-- <link rel="stylesheet" media="screen" type="text/css" -->
 <!-- 	href="./aero.css" /> -->
 <title>COMPETENCE</title>
@@ -9,15 +13,19 @@
 
 
 <?php
+include ("../db/Connexion.php");
+// $test="bonjour tout le monde";
+// echo $test;
 
-$test="bonjour tout le monde";
-echo $test;
+// $daoStagiaire = new \DAO\Stagiaire\StagiaireDAO ();
+// $stag=$daoStagiaire->read(1);
+// echo "avant: $stag";
+// $daoStagiaire->delete($stag);
+// echo "effacé : $stag";
 
-$daoStagiaire = new \DAO\Stagiaire\StagiaireDAO ();
-$stag=$daoStagiaire->read(1);
-echo "avant: $stag";
-$daoStagiaire->delete($stag);
-echo "effacé : $stag";
+
+
+echo Connexion::getTableauCompetences(1);
 
 ?>
 
